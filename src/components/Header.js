158e5cfa13fe103346e71949dcff24816a62443b
ahmetakyapi <ahmetakyapii@gmail.com>
2021-05-31 21:23:31 +0300

    Created Header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
-
 function Header() {
     return (
         <div>
+            <div id="img">
+                <img src="https://www.kindpng.com/picc/m/298-2989244_good-bmi-hd-png-download.png" alt="bmi-photo"/>
+            </div>
             <h1>
                 BMI-CALCULATOR
             </h1>
