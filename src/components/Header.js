543381b693bf2d16f9c6b95be9f4930f182100d3
ahmetakyapi <ahmetakyapii@gmail.com>
2021-06-01 14:17:34 +0300

    Update css

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ function Header()
     return (
         <div>
             <div id="img">
-                <img src="https://www.kindpng.com/picc/m/298-2989244_good-bmi-hd-png-download.png" aria-hidden alt="bmi photo" />
+                <img src="https://www.kindpng.com/picc/m/298-2989244_good-bmi-hd-png-download.png" aria-hidden alt="bmi photo" className="responsive"/>
             </div>
             <h1>
                 BMI-CALCULATOR
